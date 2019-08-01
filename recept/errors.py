90c962848193736318da323ff824a58f0b9f9278
Viktor Kerkez <alefnula@gmail.com>
2019-08-01 20:55:01 +0200

    Hide click. It's a temporary implementation detail.

--- recept/errors.py
+++ recept/errors.py
@@ -1,15 +1,20 @@
-class ReceptError(Exception):
-    def __init__(self, message):
-        self.message = message
+import click
 
-    @property
-    def name(self):
-        return self.__class__.__name__
 
-    def __str__(self):
-        return f"{self.name}({self.message})"
-
-    __repr__ = __str__
+class ReceptError(click.ClickException):
+    pass
+    # TODO: Revert when we replace click.
+    # def __init__(self, message):
+    #     self.message = message
+    #
+    # @property
+    # def name(self):
+    #     return self.__class__.__name__
+    #
+    # def __str__(self):
+    #     return f"{self.name}({self.message})"
+    #
+    # __repr__ = __str__
 
 
 class ImproperlyConfigured(ReceptError):
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import io
 from setuptools import setup, find_packages
 
 
-version = "0.0.7"
+version = "0.0.8"
 author = "Viktor Kerkez"
 author_email = "alefnula@gmail.com"
 url = "https://github.com/alefnula/recept"
